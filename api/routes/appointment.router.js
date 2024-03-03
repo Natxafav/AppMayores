7d6191131c49e6653953d210e348c77f249609e5
Natxafav <natidevg@gmail.com>
2024-03-03 17:54:42 +0000

    General: -Apointments routes added -Apointments relationships added

---
+++ api/routes/appointment.router.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+
+const {getAllAppointments,
+    getOneAppointment, 
+    createAppointment, 
+    updateAppointment, 
+    deleteAppointment}= require('../controllers/appointment.controller')
+
+router.get('/', getAllAppointments)
+router.get('/:id', getOneAppointment)
+router.post('/', createAppointment)
+router.put('/:id', updateAppointment)
+router.delete('/:id', deleteAppointment)
+
+module.exports = router
--- database/relationships.js
+++ database/relationships.js
@@ -2,7 +2,7 @@ const UserModel = require('../api/models/user.model')
 const FamilyModel = require('../api/models/family.model')
 const UserFamilyModel = require ('../api/models/user_family.model')
 const MedicationModel = require('../api/models/medication.model')
-
+const AppointmentModel = require('../api/models/appointment.model')
 const createRelationShips = () => {
     UserModel.belongsToMany(FamilyModel,{ through: UserFamilyModel, as: 'family' })
     FamilyModel.belongsToMany(UserModel, { through: UserFamilyModel, as: 'family' })
@@ -11,6 +11,8 @@ const createRelationShips = () => {
     MedicationModel.belongsTo(UserModel)
     UserModel.hasMany(MedicationModel)
 
+    AppointmentModel.belongsTo(UserModel)
+    UserModel.hasMany(AppointmentModel)    
 
 }
 module.exports = createRelationShips
