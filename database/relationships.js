d4e9a78a2ca52fa75abc56d01db159ad68835bc6
AlbertOR0994 <alberto.ocete.rosario@080formacion.alzago.es>
2024-03-04 10:08:53 +0000

    -relationships modify

--- database/relationships.js
+++ database/relationships.js
@@ -7,9 +7,11 @@ const ReminderModel = require('../api/models/reminder.model')
 const createRelationShips = () => {
     UserModel.belongsToMany(FamilyModel,{ through: UserFamilyModel, as: 'family' })
     FamilyModel.belongsToMany(UserModel, { through: UserFamilyModel, as: 'family' })
-    RoleModel.hasMany(UserModel)
-    UserModel.belongsTo(RoleModel)
-    UserModel.hasOne(ReminderModel)
+    
+    UserModel.hasMany(RoleModel)
+    RoleModel.belongsTo(UserModel)
+
+    UserModel.hasMany(ReminderModel)
     ReminderModel.belongsTo(UserModel)
 
 }
