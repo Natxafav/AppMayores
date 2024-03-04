3610f65afe3301a326b550e50ac208cb0544eacb
Natxafav <natidevg@gmail.com>
2024-03-04 18:05:33 +0000

    -CRUD: Family, Users

---
+++ api/controllers/family.contrller.js
@@ -0,0 +1,70 @@
+const FamilyModel = require("../models/family.model")
+
+const getAllFamilies = async (req, res) =>{
+    try {
+        const families = await  FamilyModel.findAll()
+        res.status(200).json(families)
+    } catch (error) {
+        res.status(500).send('Error', error.message)
+    }
+}
+
+const getOneFamily = async (req, res) =>{
+    try {
+        const family = await FamilyModel.findByPk(req.params.id)
+        if(family){
+            res.status(200).json(family)
+        }else {
+            res.status(404).send('Family not found')
+        }
+    } catch (error) {
+        return res.status(500).send('error', error.message)
+    }
+}
+
+const createFamily = async (req, res) =>{
+    try {
+        const family = await FamilyModel.create(req.body)
+        return res.status(200).json({message: 'User created', family: family})
+    } catch (error) {
+        return res.status(500).send('Error creating family', error.message)
+    }
+}
+
+const updateFamily = async (req, res) =>{
+    try {
+        const [familyExists, family] = await FamilyModel.update(
+            req.body,{
+                return: true, 
+                where: {
+                    id: req.params.id
+                }
+            })
+        if(familyExists !== 0){
+            return res.status(200).json({
+                message: 'Family created', 
+                family:family})
+        }else {
+            return res.status(404).send('Error updating family')
+        }
+    } catch (error) {
+        return  res.status(500).send('Error', error.message)
+    }
+}
+
+const deleteFamily = async (req, res) =>{
+    try {
+        const family = await FamilyModel.destroy({
+            where:{
+                id: req.params.id
+            }
+        })
+        if(family){
+            return res.status(200).send('Family deleted')
+        }else{
+            return res.status(404).send('Family not found')
+        }
+    } catch (error) {
+        return res.status(500).send('Error', error.message)
+    }
+}
--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -9,4 +9,63 @@ const getUsers = async (req, res) => {
         res.status(500).send('Error to get users')
     }
 }
+const getOneUser = async (req, res) => {
+    try {
+        const user = await UserModel.findByPk(req.params.id)
+        if(user){
+            res.status(200).json(user)
+        }else {
+            res.status(404).send('User not found')
+        }
+    } catch (error) {
+        res.status(500).send('Error', error.message)
+    }
+}
 
+const createUser = async (req, res) =>{
+    try {
+        const newUser = await UserModel.create(req.body)
+        return res.status(200).json({message:'User created', user:user})
+    } catch (error) {
+        res.status(500).send('Error', error.message)
+    }
+}
+const updateUser = async (req, res) => {
+    try {
+        const [userExist, user]= await UserModel.update(
+            req.body, 
+            {
+                returning: true,
+                where:{
+                    id: req.params.id
+                }
+            })
+        if(userExist !== 0 ) {
+            return res.status(200).json({
+                message: 'User updated',
+                user: user
+            })
+        }else {
+            return res.status(404).send('User not found')
+        }
+    } catch (error) {
+        return res.status(500).send('Error retrieving data')
+    }
+}
+
+const deleteUser = async(req,res) => {
+    try {
+        const user = UserModel.destroy({
+            where:{
+                id:req.params.id
+            }
+        })
+        if(user){
+            return res.status(200).send('User deleted')
+        }else{
+            return res.status(404).send('User not found')
+        }
+    } catch (error) {
+        return res.status(500).send('Error', error.message)
+    }
+}
