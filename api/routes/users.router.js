e588d768f1006f520b85741e5204ef97716914ab
Natxafav <natidevg@gmail.com>
2024-03-05 15:23:50 +0000

    pull from github/Nati

--- api/routes/users.router.js
+++ api/routes/users.router.js
@@ -1,14 +1,15 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-const {getAllUsers,
-getOneUser,
-updateUser,
-deleteUser} = require('../controllers/user.controller')
+const {
+  getAllUsers,
+  getOneUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/user.controller");
 
+router.get("/get", getAllUsers);
+router.get("/one/:id", getOneUser);
+router.put("/mod/:id", updateUser);
+router.delete("/rm/:id", deleteUser);
 
-router.get('/get', getAllUsers)
-router.get('/one/:id', getOneUser)
-router.put('/mod/:id', updateUser)
-router.delete('/rm/:id', deleteUser)
-
-module.exports = router
+module.exports = router;
