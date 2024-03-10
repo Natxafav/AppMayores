5b0bb7bbc1b362f976786e0e24208907d5937c23
Natxafav <natidevg@gmail.com>
2024-03-10 17:41:42 +0000

    Current day added

--- api/controllers/reproductions.controller.js
+++ api/controllers/reproductions.controller.js
@@ -11,15 +11,21 @@ const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 const getAllRecordsForToday = async (req, res) => {
   try {
+    //! Modificar para que se pueda introducir la fecha a consultar por parámetros
+    const currentDate = new Date();
+    const startOfDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), 0, 0, 0);
+    const endOfDay = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), 23, 59, 59);
+
     const appointments = await AppointmentModel.findAll({
       where: {
         userId: res.locals.user.id,
+        datetime:{[Op.between]:[startOfDay, endOfDay]}
       },
     });
 
     for (const appointment of appointments) {
       const fullDate = appointment.datetime;
-      const onlyDate = `${fullDate.getDate()}-${fullDate.getMonth()}-${fullDate.getFullYear()} a las ${fullDate.getHours()} y ${fullDate.getMinutes()}`;
+      const onlyDate = `${fullDate.getDate()}-${fullDate.getMonth()+1}-${fullDate.getFullYear()} a las ${fullDate.getHours()} y ${fullDate.getMinutes()}`;
       const message = `Cita medica en ${appointment.locate} el dia ${onlyDate} con el ${appointment.specialist} . La descripcion del evento incluye ${appointment.description}`;
       say.speak(message)
       await delay(10000);
@@ -27,11 +33,12 @@ const getAllRecordsForToday = async (req, res) => {
     const medications = await MedicationModel.findAll({
       where: {
         userId: res.locals.user.id,
+        datetime:{[Op.between]:[startOfDay, endOfDay]}
       },
     });
     for (const medication of medications) {
         const fullDate = medication.datetime;
-        const onlyDate = `${fullDate.getDate()}-${fullDate.getMonth()}-${fullDate.getFullYear()} a las ${fullDate.getHours()} y ${fullDate.getMinutes()}`;
+        const onlyDate = `${fullDate.getDate()}-${fullDate.getMonth()+1}-${fullDate.getFullYear()} a las ${fullDate.getHours()} y ${fullDate.getMinutes()}`;
         const message = `Tienes que tomarte ${medication.name} el dia ${onlyDate}. La descripcion del evento incluye ${medication.description}`;
         say.speak(message)
         await delay(10000);
@@ -39,12 +46,13 @@ const getAllRecordsForToday = async (req, res) => {
       const reminders = await ReminderModel.findAll({
         where: {
           userId: res.locals.user.id,
+          Date:{[Op.between]:[startOfDay, endOfDay]}
         },
       });
   
       for (const reminder of reminders) {
         const fullDate = reminder.Date;
-        const onlyDate = `${fullDate.getDate()}-${fullDate.getMonth()}-${fullDate.getFullYear()} a las ${fullDate.getHours()} y ${fullDate.getMinutes()}`;
+        const onlyDate = `${fullDate.getDate()}-${fullDate.getMonth()+1}-${fullDate.getFullYear()} a las ${fullDate.getHours()} y ${fullDate.getMinutes()}`;
         const message = `Recordatorio ${reminder.name} el dia ${onlyDate} . La descripcion del evento incluye ${reminder.description}`;
         say.speak(message)
         await delay(10000);
