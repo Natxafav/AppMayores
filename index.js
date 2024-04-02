50e233a66379a2e7043ba6d72c3a199465f39aea
Nati Díaz <natxafav@gmail.com>
2024-04-02 12:01:06 +0100

    Cors

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ require('dotenv').config()
 const morgan = require('morgan')
 const createRelationShips = require('./database/relationships.js')
 const express = require('express')
+const cors = require('cors')
 const api = express()
 
 const sequelize = require('./database')
 const dbSync = require('./database/dbSync')
 
+api.use(cors())
 api.use(morgan('dev'))
 api.use(express.json())
 
