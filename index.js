14b6fd94e7c9d0bda18055c7ebd9ceef69614b4e
Natxafav <natidevg@gmail.com>
2024-04-14 02:19:08 +0100

    mergge dev

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const dbCheck = async (req, res) => {
 try {
     await sequelize.authenticate();
     createRelationShips()
-   await dbSync();
+   //await dbSync();
 } catch (error) {
     console.log(error.message)
     throw new Error(error)
